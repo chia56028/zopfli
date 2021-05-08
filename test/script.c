d996a7870df9c40788ffb4629b78a71b8299f6f1
5fe6eb50c7aa19f9 <51sf1hgd@gmail.com>
2021-05-08 12:14:12 +0800

    add a sample test_case

--- test/script.c
+++ test/script.c
@@ -7,6 +7,29 @@
 #include "gzip_container.h"
 #include "zlib_container.h"
 
+void testZopfliDeflate(){
+	ZopfliOptions options;
+	ZopfliInitOptions(&options);//util.c
+	//options.verbose=1;
+	int btype=2;//the block type
+	int final=1;//whether to set the "final" bit on this block, must be the last block
+	unsigned char in[10000];
+	size_t insize=10000;
+	unsigned char bp=0;
+	unsigned char* out=0;
+	size_t outsize=0;
+
+	ZopfliDeflate(&options,btype,final,in,insize,&bp,&out,&outsize);
+	//printf("[TEST] insize: %lu, bp: %d, outsize: %lu\n",insize,bp,outsize);
+
+	assert(outsize!=0);
+	assert(out!=0);
+	free(out);
+}
+
 int main(){
-	printf("123\n");
+	//call your test function here
+	testZopfliDeflate();
+
+	printf("[TEST] All done.\n");
 }
