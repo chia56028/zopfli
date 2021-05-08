9deaacf8eb6fee8c4db1a9dc7270b9e6a2a9e295
5fe6eb50c7aa19f9 <51sf1hgd@gmail.com>
2021-05-08 11:13:43 +0800

    test script entry

---
+++ test/script.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "deflate.h"
+#include "gzip_container.h"
+#include "zlib_container.h"
+
+int main(){
+	printf("123\n");
+}
